0dabe1e29bc1681b1fc4d5c1c6f36869e56e8fcd
Dustin <dustin.price8908@gmail.com>
2023-05-03 16:08:14 -0400

    Added RewardValue class.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+public class RewardValue {
+    private double cash;
+    private int miles;
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+        miles = (int)(cash / 0.0035);
+    }
+
+    public RewardValue(int miles) {
+        this.miles = miles;
+        cash = (miles * 0.0035);
+    }
+
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return miles;
+    }
+}
